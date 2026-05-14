10fd2ac37b76e5e698e650989376f3ef904e5d69
kazekagyeee <kazekagyee@gmail.com>
2026-05-14 23:04:33 +0500

    fix: мелкофикс запроса в бд

--- db.py
+++ db.py
@@ -143,7 +143,8 @@ def search_image_chunks(text_chunk_ids, query_embedding):
     cur.execute("""
         SELECT image_path, bbox FROM image_chunks
         WHERE text_chunk_id IN %s
-        ORDER BY embedding <-> %s DESC;
+        ORDER BY embedding <-> %s DESC
+        LIMIT 1;
     """, (tuple(text_chunk_ids), query_arr))
     
     rows = cur.fetchall()
